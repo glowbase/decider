40fabb8955e97249ee7ed1142f64ba5b8624d80b
Allan Korol <allan.korol@gmail.com>
2024-08-14 14:12:18 +1000

    Removed magic values for file paths.
    Added versioning for mitigations in line with existing attack_version methods.
    This ensures that mitigations are tied to specific techniques of an attack version, even though the current import is only for v15.1

--- app/constants.py
+++ app/constants.py
@@ -2,3 +2,5 @@ import os
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
 BUILD_SOURCES_DIR = os.path.join(current_dir, "../config/build_sources")
+mitigations_mapping_dir = "mappings"
+mitigations_mapping_file_base = "mappings"
